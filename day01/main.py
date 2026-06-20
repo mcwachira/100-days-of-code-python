a9786db0e2c9d36cfbd5af85d5b4c21c6bc73f25
mcwachira <mcwachira@outlook.com>
2026-06-20 10:28:06 +0300

    added changes to make cursor appear on a newline

--- day01/main.py
+++ day01/main.py
@@ -1,5 +1,5 @@
 print("Welcome to the band generator")
-name = input("what City did you grow up in ?")
-pet = input("What is the name of your pet?")
+name = input("what City did you grow up in ?\n")
+pet = input("What is the name of your pet?\n")
 
 print( name + pet)
