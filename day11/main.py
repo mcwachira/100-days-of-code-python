0f66722747768560bead2a7d1bdfee5d998e4b72
mcwachira <mcwachira@outlook.com>
2026-07-15 12:17:00 +0300

    made changes to the game in  order to separate the logic and make it overall better.

--- day11/main.py
+++ day11/main.py
@@ -2,14 +2,18 @@ import random
 from art import logo
 print(logo)
 
+# Creat a deck of cards
+def create_deck():
+    # 4 sets of cards (like 4 suits)
+    return [11,2,3,4,5,6,7,8,9,10,10,10,10]*4
 
+# deal one card
 
-def dealCards():
-    # Card values (Ace = 11 for now)
-    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-    card  = random.choice(cards)
-    return card
+def deal_card(deck):
+    # Remove and return the top card from the deck
+    return deck.pop()
 
+# calculate score
 def calculateScore(cards):
     total = sum(cards)
     while total> 21 and 11 in cards:
@@ -19,91 +23,103 @@ def calculateScore(cards):
         total = sum(cards)
     return total
 
-play_again = True
-while play_again:
 
-    player_cards = []
-    pc_cards = []
+# players turn to play
+def player_turn(deck, player_cards):
+    while True:
+        player_score = calculateScore(player_cards)
+        print(f"\nYour cards: {player_cards} | Score: {player_score}")
 
-    # Ask player if they want to start
-    value = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+        #check if player won or lost
+        if (player_score > 21):
+            print("You went over 21. You lose!")
+            return player_score
+        elif (player_score == 21):
+            print("Blackjack! You win!")
+            return player_score
 
-    if value == 'y':
+        # Ask for input
+        choice = input("Type 'y' to hit or 'n' to stand: ").lower()
 
-        #deal 2 cards to the player and pc at the same time
-        for _ in range(2):
-            player_cards.append(dealCards())
-            pc_cards.append(dealCards())
-
-        print(f"Your cards :{player_cards}")
-        print(f"Pc first cards :{pc_cards[0]}")
-
-        # ---------------- PLAYER TURN ----------------
-        player_turn = True
-
-        while player_turn:
-            player_score =sum(player_cards)
-            if(sum(player_cards) > 21):
-                print("You went over 21. You lose!")
-                break
-            elif(sum(player_cards) == 21):
-                print("Blackjack! You win!")
-                break
-
-            # Ask player if they want another card
-            value = input("Do you want to get another card? Type 'y' or 'n':").lower()
-            if value == 'y':
-
-                # add one new card
-                new_card = dealCards()
-                player_cards.append(new_card)
-                print(f"You drew: {new_card}")
-                print(f"Your cards: {player_cards}")
-
-            else:
-                #players turn is false
-                player_turn = False
-
-            # ---------------- DEALER TURN ----------------
-            # Only run if player didn't bust
-
-                if(sum(player_cards) <21):
-                    print(f"\nComputer's cards: {pc_cards}")
-                #dealer must draw until reaching 17 or higher
-
-                    while sum(pc_cards) <17 :
-                        new_card = dealCards()
-                        pc_cards.append(new_card)
-
-                        print(f"Computer draws: {new_card}")
-
-                    print(f"Computer's final hand: {pc_cards}")
-
-                    # ---------------- RESULT ----------------
-                    player_score =calculateScore(player_cards)
-                    pc_score = calculateScore(pc_cards)
-
-                    if pc_score > 21:
-                        print("Computer went over 21. You win!")
-                    elif player_score > pc_score:
-                        print("You win!")
-                    elif player_score < pc_score:
-                        print("You lose!")
-                    else:
-                        print("It's a draw!")
+        if choice == 'y':
+            # Draw a new card
+            new_card = deal_card(deck)
+            player_cards.append(new_card)
+            print(f"You drew: {new_card}")
+        elif choice == 'n':
+            # End player turn
+            return player_score
+        else:
+            print("Invalid input. Try again.")
 
 
-    elif value == 'n':
 
-        # Exit immediately if player doesn't want to start
+# dealers turn
+def dealer_turn(deck, dealer_cards):
+    print(f"\nDealer's cards: {dealer_cards}")
 
+    #Dealer must draw until the reach at least 17
+    while calculateScore(dealer_cards) < 17:
+        new_card = deal_card(deck)
+        dealer_cards.append(new_card)
+        print(f"Dealer drew: {new_card}")
+
+    dealer_score = calculateScore(dealer_cards)
+    print(f"Dealer's final hand: {dealer_cards} | Score: {dealer_score}")
+    return dealer_score
+
+
+
+
+# compare scores
+def compare_scores(player_score, dealer_score):
+    if player_score > 21:
+        print("You lose!")
+    elif dealer_score > 21:
+        print("Dealer went over. You win!")
+    elif player_score > dealer_score:
+        print("You win!")
+    elif player_score < dealer_score:
+        print("You lose!")
+    else:
+        print("It's a draw!")
+
+
+# play one game
+
+def play_game():
+    #create and shuffle the  dec
+    deck = create_deck()
+    random.shuffle(deck)
+
+    # Deal initial hands
+    player_cards= [deal_card(deck), deal_card(deck)]
+    dealer_cards = [deal_card(deck), deal_card(deck)]
+
+    # Show initial cards (hide dealer second card)
+    print(f"\nYour cards: {player_cards}")
+    print(f"Dealer's first card: {dealer_cards[0]}")
+
+    # Player turn
+    player_score = player_turn(deck, player_cards)
+    #Dealer turn ONLY if player didn't bust or hit blackjack
+    if player_score <= 21 and player_score != 21:
+        dealer_score = dealer_turn(deck, dealer_cards)
+    else: dealer_score = calculateScore(dealer_cards)
+    # Compare final scores
+    compare_scores(player_score, dealer_score)
+
+
+
+#MAIN GAME LOOP
+
+while True:
+    start = input("\nDo you want to play Blackjack? (y/n): ").lower()
+
+    if start == 'y':
+        play_game()
+    elif start == 'n':
+        print("Thanks for playing!")
         break
-
-        # ---------------- PLAY AGAIN ----------------
-
-    again = input("\nDo you want to play again? (y/n): ").lower()
-
-    if again != "y":
-        play_again = False
-
-        print("Thanks for playing blackjack!")
+    else:
+        print("Invalid input. Try again.")
